a324695b141a4b25e586e930722c66b3afadf3e9
SleepyKraken <59947759+SleepyKraken@users.noreply.github.com>
2020-02-20 22:18:33 -0600

    Added Audio and Input Conditions
    +Audio Clip for Rocket
    +If Statement for Audio Clip

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -8,17 +8,25 @@ public class Rocket : MonoBehaviour
 
     Rigidbody rigidBody;
 
+    AudioSource audioSource;
+    bool m_ToggleChange;
+    
+
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+
+        audioSource = GetComponent<AudioSource>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         ProcessInput();
+
     }
 
     private void ProcessInput()
@@ -26,17 +34,28 @@ public class Rocket : MonoBehaviour
         if (Input.GetKey(KeyCode.Space)) //space bar == thrust key
         {
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
         }
 
-        if (Input.GetKey(KeyCode.A))
+        else 
+        {
+            audioSource.Stop();
+        }
+
+        if (Input.GetKey(KeyCode.A)) //pivot left
         {
             transform.Rotate(Vector3.forward);
         }
 
-        else if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D)) // pivot right
         {
             transform.Rotate(-Vector3.forward);
         }
 
+        
     }
+
 }
