a9e02be0a7f736927001c0ba646275a656cc89f8
SleepyKraken <59947759+SleepyKraken@users.noreply.github.com>
2020-02-24 23:43:12 -0600

    Added Oscilator Script to Game Object
    Moving Wall config

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -2,28 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[DisallowMultipleComponent]
+[DisallowMultipleComponent] //<-- Attribute. Only allows one script on the game object
 public class Oscillator : MonoBehaviour
 
 
 {
 
-    [SerializeField] Vector3 movementVector;
-
-    //todo remove from inspector later
-    [SerializeField] [Range(0,1)]
-    float movementFactor;
+    [SerializeField] Vector3 movementVector; //Vector3 = Transform 'postions' X, Y, Z
     
+    //todo remove from inspector later
+    [SerializeField] [Range(0,1)] //0 for not moved, 1 for full movement
+    float movementFactor;
+
+    Vector3 startingPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        startingPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPos + offset;
     }
 }
